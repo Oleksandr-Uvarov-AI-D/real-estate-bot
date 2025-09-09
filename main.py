e46c1979e374e8c3c890157cdf20f2a66ccf63b5
Oleksandr Uvarov <oleksandruvarov@Oleksandrs-MacBook-Pro.local>
2025-09-09 12:41:29 +0200

    Trying to connect Formspree to render.

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI, Request
 from fastapi.middleware.cors import CORSMiddleware
-from fastapi.responses import HTMLResponse
+from fastapi.responses import HTMLResponse, JSONResponse
 from dotenv import load_dotenv 
 
 load_dotenv()
@@ -30,7 +30,19 @@ def home_chat():
 
 @app.post("/formspree")
 async def send_form(request: Request):
+    print(request)
     data = await request.json()
-
     print(data)
-    return data
+
+    hook_secret = request.headers.get("X-Hook-Secret")
+
+    if hook_secret:
+        return JSONResponse(
+            content={"message": "Webhook verifieed"},
+            headers={"X-Hook-Secret": hook_secret}
+            status_code=200
+        )
+
+    data = await request.json()
+    print(data)
+    return {"message": "Webhook received"}
