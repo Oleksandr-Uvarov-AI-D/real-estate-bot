8140b98874e84c7032e40564fb62174f28183199
Oleksandr Uvarov <oleksandruvarov@Oleksandrs-MacBook-Pro.local>
2025-09-09 12:04:26 +0200

    Added /formspree POST method.

---
+++ main.py
@@ -0,0 +1,23 @@
+from fastapi import FastAPI, Request
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import HTMLResponse
+from dotenv import load_dotenv 
+
+load_dotenv()
+
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_method=["*"],
+    allow_headers=["*"]
+)
+
+@app.post("/formspree")
+async def send_form(request: Request):
+    data = await request.json()
+
+    return data
