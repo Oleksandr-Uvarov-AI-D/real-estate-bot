dc91011de1ed58b2d886bc12727e10ccc007c133
Oleksandr Uvarov <oleksandr@ai-d.be>
2025-09-12 12:22:21 +0200

    fixing an order error.

--- cal_com_methods.py
+++ cal_com_methods.py
@@ -39,10 +39,12 @@ def try_to_make_an_appointment(chatbot_message):
         # Or it's a dict which will be used to fill in data for the appointment further down in this method.
         message_json = extract_json(message)
 
+
+        start = message_json["start"]
         available_slots = get_days_and_times(event_type_id, start, language=language)
 
         name, email, phone_number= message_json["name"], message_json["email"], message_json["phone_number"]
-        start, language, msg = message_json["start"], "nl", f"Je afspraak voor {available_slots[2]} is succesvol ingepland. We nemen spoedig contact met je op."
+        language, msg = "nl", f"Je afspraak voor {available_slots[2]} is succesvol ingepland. We nemen spoedig contact met je op."
         status_code = book_cal_event(name, email, phone_number, start, language)
         if status_code == 400:
             if language == "en":
